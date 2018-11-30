68972d19840597d7b7f58c39614e5c402cfe6f17
libo <choulibo@gmail.com>
2018-11-30 16:44:10 +0800

    极验验证&头条

--- 05_geetest_jiyanyanzheng.py
+++ 05_geetest_jiyanyanzheng.py
@@ -19,11 +19,12 @@ INIT_LEF = 60
 
 class Geetest():
     """极验验证码"""
+
     # 初始化
     def __init__(self):
         self.url = "https://account.geetest.com/login"
         self.browser = webdriver.Chrome()
-        self.wait = WebDriverWait(self.browser,20)
+        self.wait = WebDriverWait(self.browser, 20)
         self.email = EMAIL
         self.password = PASSWORD
 
@@ -37,7 +38,7 @@ class Geetest():
         :return: 按钮对象
         """
 
-        button = self.wait.until(EC.element_to_be_clickable(By.CLASS_NAME,'geetest_radar_tip'))
+        button = self.wait.until(EC.element_to_be_clickable(By.CLASS_NAME, 'geetest_radar_tip'))
         return button
 
     def get_screenshot(self):
@@ -49,31 +50,31 @@ class Geetest():
         screenshot = Image.open(BytesIO(screenshot))
         return screenshot
 
-
     def get_position(self):
         """
         获取验证码位置
         :return: 验证码位置元组
         """
 
-        img = self.wait.until(EC.presence_of_element_located((By.CLASS_NAME,'geetest_canvas_img')))
+        img = self.wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'geetest_canvas_img')))
         time.sleep(2)
         location = img.location
         size = img.size
-        top,bottom,left,right =location['y'],location['y'] + size['height'],location['x'],location['x']+size['width']
-        return (top,bottom,left,right)
+        top, bottom, left, right = location['y'], location['y'] + size['height'], location['x'], location['x'] + size[
+            'width']
+        return (top, bottom, left, right)
 
-    def get_geetest_image(self,name='captcha.png'):
+    def get_geetest_image(self, name='captcha.png'):
         """
         获取验证码图片
         :param name:
         :return: 图片对象
         """
 
-        top,bottom,left,right = self.get_position()
-        print("验证码位置",top,bottom,left,right)
+        top, bottom, left, right = self.get_position()
+        print("验证码位置", top, bottom, left, right)
         screenshot = self.get_screenshot()
-        captcha = screenshot.crop((left,top,right,bottom))
+        captcha = screenshot.crop((left, top, right, bottom))
         return captcha
 
     def get_slider(self):
@@ -81,10 +82,10 @@ class Geetest():
         获取滑块
         :return: 滑块对象
         """
-        slider = self.until(EC.element_to_be_clickable((By.CLASS_NAME,'geetest_slider_button')))
+        slider = self.until(EC.element_to_be_clickable((By.CLASS_NAME, 'geetest_slider_button')))
         return slider
 
-    def is_pixel_equal(self,image1,image2,x,y):
+    def is_pixel_equal(self, image1, image2, x, y):
         """
         判断两个像素是否相同
         :param image1: 图片1
@@ -94,10 +95,11 @@ class Geetest():
         :return:像素是否相同
         """
         # 取两个图片的像素点
-        pixel1 = image1.load()[x,y]
-        pixel2 = image2.load()[x,y]
+        pixel1 = image1.load()[x, y]
+        pixel2 = image2.load()[x, y]
         threshold = 60
-        if abs(pixel1[0]-pixel2)< threshold and abs(pixel1[1]-pixel2[1]) < threshold and abs(pixel1[2]-pixel2[2])<threshold:
+        if abs(pixel1[0] - pixel2) < threshold and abs(pixel1[1] - pixel2[1]) < threshold and abs(
+                pixel1[2] - pixel2[2]) < threshold:
             return True
         else:
             return False
@@ -108,13 +110,12 @@ class Geetest():
         :return:
         """
         self.browser.get(self.url)
-        email = self.wait.until(EC.presence_of_element_located((By.ID,'email')))
-        password = self.wait.until(EC.presence_of_element_located((By.ID,'password')))
+        email = self.wait.until(EC.presence_of_element_located((By.ID, 'email')))
+        password = self.wait.until(EC.presence_of_element_located((By.ID, 'password')))
         email.send_keys(self.email)
         password.send_keys(self.password)
 
-
-    def get_gap(self,image1,image2):
+    def get_gap(self, image1, image2):
         """
         获取缺口偏移量
         :param image1: 不带缺口图片
@@ -122,14 +123,14 @@ class Geetest():
         :return:
         """
         left = 60
-        for i in range(left,image1.size[0]):
+        for i in range(left, image1.size[0]):
             for j in range(image2[1]):
-                if not self.is_pixel_equal(image1,image2,i,j):
+                if not self.is_pixel_equal(image1, image2, i, j):
                     left = i
                     return left
         return left
 
-    def get_track(self,distance):
+    def get_track(self, distance):
         """
         根据偏移量获取移动轨迹
         :param distance: 偏移量
@@ -140,14 +141,14 @@ class Geetest():
         # 当前位移
         current = 0
         # 减速阈值
-        mid = distance*4/5
+        mid = distance * 4 / 5
         # 计算间隔
         t = 0.2
         # 初速度
         v = 0
         # 当前位移小于总距离
-        while current< distance:
-            if current<mid:
+        while current < distance:
+            if current < mid:
                 # 加速度为正2
                 a = 2
             else:
@@ -155,16 +156,16 @@ class Geetest():
             # 初速度是v0
             v0 = v
             # 当前速度
-            v = v0 +a*t
+            v = v0 + a * t
             # 移动距离
-            move = v0*t +0.5*a*t*t
+            move = v0 * t + 0.5 * a * t * t
             # 当前位移
             current += move
             # 加入轨迹
             track.append(round(move))
         return track
 
-    def move_to_gap(self,slider,tracks):
+    def move_to_gap(self, slider, tracks):
         """
         拖动滑块到缺口处
         :param slider:滑块
@@ -173,7 +174,7 @@ class Geetest():
         """
         ActionChains(self.browser).click_and_hold(slider).perform()
         for x in tracks:
-            ActionChains(self.browser).move_by_offset(xoffset=x,yoffset=0).perform()
+            ActionChains(self.browser).move_by_offset(xoffset=x, yoffset=0).perform()
         time.sleep(0.5)
         ActionChains(self.browser).release().perform()
 
@@ -182,7 +183,7 @@ class Geetest():
         登录
         :return: None
         """
-        submit = self.wait.until(EC.element_to_be_clickable((By.CLASS_NAME,'login-btn')))
+        submit = self.wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'login-btn')))
         submit.click()
         time.sleep(10)
         print("登录成功")
@@ -201,17 +202,18 @@ class Geetest():
         # 获取带缺口的验证码图片
         image2 = self.get_geetest_image('captcha.png')
         # 获取缺口位置
-        gap = self.get_gap(image1,image2)
-        print("缺口位置",gap)
+        gap = self.get_gap(image1, image2)
+        print("缺口位置", gap)
         # 减去缺口位移
         gap -= BORDER
         # 获取移动轨迹
         track = self.get_track(gap)
-        print("滑动轨迹",track)
+        print("滑动轨迹", track)
         # 拖动滑块
-        self.move_to_gap(slider,track)
+        self.move_to_gap(slider, track)
 
-        success = self.wait.until(EC.text_to_be_present_in_element((By.CLASS_NAME,'geetest_success_radar_tip_content'),'验证成功'))
+        success = self.wait.until(
+            EC.text_to_be_present_in_element((By.CLASS_NAME, 'geetest_success_radar_tip_content'), '验证成功'))
         print(success)
         # 失败后重试
         if not success:
@@ -219,14 +221,7 @@ class Geetest():
         else:
             self.login()
 
+
 if __name__ == '__main__':
     crack = Geetest()
     crack.crack()
-
-
-
-
-
-
-
-
--- toutiao.py
+++ toutiao.py
@@ -7,14 +7,16 @@ import os
 from hashlib import md5
 from urllib.parse import urlencode
 from multiprocessing import Pool
+
+
 def get_page(offset):
     """单页"""
     params = {
-        "offset":offset,
-        "format":"json",
-        "keyword":"街拍",
-        "count":"20",
-        "curtab":"3"
+        "offset": offset,
+        "format": "json",
+        "keyword": "街拍",
+        "count": "20",
+        "curtab": "3"
     }
 
     url = "https://www.toutiao.com/search_content/?" + urlencode(params)
@@ -25,6 +27,7 @@ def get_page(offset):
     except requests.ConnectionError:
         return None
 
+
 def get_images(json):
     if json.get("data"):
         for item in json.get("data"):
@@ -32,8 +35,8 @@ def get_images(json):
             images = item.get("image_list")
             for image in images:
                 yield {
-                    "image":image.get("url"),
-                    "title":title
+                    "image": image.get("url"),
+                    "title": title
                 }
 
 
@@ -43,20 +46,20 @@ def save_image(item):
     try:
         response = requests.get(item.get("image"))
         if response.status_code == 200:
-            file_path = "{}/{}.{}".format(item.get("title"),md5(response.content).hexdigest(),"jpg")
+            file_path = "{}/{}.{}".format(item.get("title"), md5(response.content).hexdigest(), "jpg")
             print(file_path)
             if not os.path.exists(file_path):
-                with open('file_path',"wb") as f:
+                with open('file_path', "wb") as f:
                     f.write(response.content)
             else:
-                print("Done",file_path)
+                print("Done", file_path)
     except requests.ConnectionError:
         print("Failed")
 
 
 def main(offset):
     json = get_page(offset)
-    for item  in get_images(json):
+    for item in get_images(json):
         # print(item)
         save_image(item)
 
@@ -64,11 +67,9 @@ def main(offset):
 GROUP_START = 1
 GROUP_END = 20
 
-
-
 if __name__ == '__main__':
     pool = Pool()
-    groups = ([x*20 for x in range(GROUP_START,GROUP_END)])
-    pool.map(main,groups)
+    groups = ([x * 20 for x in range(GROUP_START, GROUP_END)])
+    pool.map(main, groups)
     pool.close()
     pool.join()
